67acac1e89f206e515b9b563a2d0ddaae750ed09
Rafael Sana Montevechio <rafael_sana@hotmail.com>
2022-01-18 08:48:41 -0300

    next auth

---
+++ Contexts/AuthContext.tsx
@@ -0,0 +1,83 @@
+import { createContext, ReactNode, useEffect, useState } from 'react';
+
+import Router from 'next/router';
+
+import { setCookie, parseCookies } from 'nookies'; // lib utilizada para armazenar informações no cookie do navegador
+
+import { api } from '../services/api';
+
+type User = {
+  email: string;
+  permissions: string[];
+  roles: string[];
+};
+
+type SignInCredentials = {
+  email: string;
+  password: string;
+};
+
+type AuthContextData = {
+  signIn(credentials: SignInCredentials): Promise<void>;
+  isAuthenticated: boolean;
+  user: User | undefined;
+};
+
+type AuthProviderProps = {
+  children: ReactNode;
+};
+
+export const AuthContext = createContext({} as AuthContextData);
+
+export function AuthProvider({ children }: AuthProviderProps) {
+  const [user, setUser] = useState<User>();
+  const isAuthenticated = !!user;
+
+  useEffect(() => {
+    const { 'nextauth.token': token } = parseCookies();
+
+    if (token) {
+      api.get('/me').then((response) => {
+        const { email, permissions, roles } = response.data;
+
+        setUser({ email, permissions, roles });
+      });
+    }
+  }, []);
+
+  const signIn = async ({ email, password }: SignInCredentials) => {
+    try {
+      const response = await api.post('sessions', {
+        email,
+        password,
+      });
+
+      const { token, refreshToken, permissions, roles } = response.data;
+
+      // Setando dados no cookie do navegador
+      setCookie(undefined, 'nextauth.token', token, {
+        maxAge: 60 * 60 * 24 * 30, // 30 dias
+        path: '/',
+      });
+
+      setCookie(undefined, 'nextauth.refreshToken', refreshToken, {
+        maxAge: 60 * 60 * 24 * 30, // 30 dias
+        path: '/',
+      });
+
+      setUser({ email, permissions, roles });
+
+      api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+
+      Router.push('/dashboard');
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <AuthContext.Provider value={{ signIn, isAuthenticated, user }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
