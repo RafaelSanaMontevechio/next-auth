4089b466947f31bda9094138d15563a49ec6b1f3
Rafael Sana Montevechio <rafael_sana@hotmail.com>
2022-02-09 08:40:48 -0300

    feat: :sparkles: token in server side

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
-import type { NextPage } from 'next';
+import type { GetServerSideProps, NextPage } from 'next';
 import { FormEvent, useContext, useState } from 'react';
 import { AuthContext } from '../Contexts/AuthContext';
+
+import { parseCookies } from 'nookies';
 
 import styles from '../styles/Home.module.css';
 
@@ -34,6 +36,23 @@ const Home: NextPage = () => {
       <button type="submit">Entrar</button>
     </form>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const cookies = parseCookies(ctx);
+
+  if (cookies['nextauth.token']) {
+    return {
+      redirect: {
+        destination: '/dashboard',
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
 };
 
 export default Home;
